c1437fb1414d889eb02448f372e5eba2c0fb5ecc
Moses Valenzuela <developinghephepaestus@gmail.com>
2023-11-25 23:17:50 -0800

    completed the challenge

---
+++ script.js
@@ -0,0 +1,58 @@
+//------------------- Global Variables -------------------
+
+let ratingSelection;
+let counter = 0;
+let colorState;
+
+function ratingUpdate() {
+
+// Function Variables
+    let rating = document.getElementById("rating");
+    let secondContainer = document.getElementById("secondary");
+    let mainContainer =document.getElementById("main");
+
+// Update rating on secondary page
+    if (ratingSelection <= 5 || ratingSelection >=1) {
+
+        rating.innerText = ratingSelection;
+    } else {
+
+        alert("Error");
+    };
+
+// hide and view logic for multi-page HTML doc
+    secondContainer.style.visibility = "visible";
+    secondContainer.style.display = "flex";
+    mainContainer.style.visibility = "hidden";
+    mainContainer.style.display = "none";
+};
+
+function saveElement(y) {
+
+//Fuction Variables
+
+    let btnSelected = document.getElementsByClassName("btn-choice")[y-1];
+    let orange = "hsl(25, 97%, 53%)";
+    let white = "hsl(0, 0%, 100%)";
+
+//Color Logic
+
+    if(colorState === undefined) {
+
+        btnSelected.style.backgroundColor = orange;
+        btnSelected.style.color = white;
+        colorState = btnSelected;
+
+    } else {
+
+        colorState.style.backgroundColor = "";
+        colorState.style.color = "";
+
+        btnSelected.style.backgroundColor = orange;
+        btnSelected.style.color = white;
+        colorState = btnSelected;
+    };
+
+// State management
+    ratingSelection = y;
+};
